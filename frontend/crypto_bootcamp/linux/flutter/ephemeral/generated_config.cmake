2f7c30b095ddf721644ba7f65b596f4e0c010318
Siddarth <sjnraja0209@gmail.com>
2022-04-26 01:05:29 +0800

    App code midst

---
+++ frontend/crypto_bootcamp/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/siddarth/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/siddarth/crypto-tech/frontend/crypto_bootcamp" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/siddarth/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/siddarth/crypto-tech/frontend/crypto_bootcamp"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/siddarth/crypto-tech/frontend/crypto_bootcamp/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/siddarth/crypto-tech/frontend/crypto_bootcamp/lib/main.dart"
+)
